21d08cfce638d6cde9d981a7b09eb74a7082c1d4
Mason <esslinger.mason@proton.me>
2025-02-17 12:32:38 -0700

    fix insert mode tab shortcuts

--- nvim/after/ftplugin/tex.lua
+++ nvim/after/ftplugin/tex.lua
@@ -1,14 +1,5 @@
-vim.opt_local.comments = 'b:*,b:- [ ],b:-,b:+,n:>'
-vim.opt_local.formatoptions:append('r')
-
-vim.opt_local.shiftwidth  = 2
-vim.opt_local.softtabstop = 2
-
 vim.keymap.set({ 'n', 'v' }, 'j', 'gj', { buffer = true })
 vim.keymap.set({ 'n', 'v' }, 'k', 'gk', { buffer = true })
 vim.keymap.set({ 'n', 'v' }, '^', 'g^', { buffer = true })
 vim.keymap.set({ 'n', 'v' }, '$', 'g$', { buffer = true })
 vim.keymap.set({ 'n', 'v' }, '0', 'g0', { buffer = true })
-
-vim.keymap.set('i', '<Tab>',   '<C-t>', { buffer = true })
-vim.keymap.set('i', '<S-Tab>', '<C-d>', { buffer = true })
