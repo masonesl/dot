fdd8a69e6eb6c3434432a9751151fe24bdd83560
Mason <esslinger.mason@proton.me>
2025-11-14 03:55:46 -0700

    small harpoon refactor

--- nvim/lua/config/harpoon.lua
+++ nvim/lua/config/harpoon.lua
@@ -5,11 +5,9 @@ vim.keymap.set('n', '<Leader>ha', function () harpoon:list():add() end)
 vim.keymap.set('n', '<Leader>hr', function () harpoon:list():remove() end)
 vim.keymap.set('n', '<Leader>hc', function () harpoon:list():clear() end)
 
-vim.keymap.set('n', '<Leader>h1', function () harpoon:list():select(1) end)
-vim.keymap.set('n', '<Leader>h2', function () harpoon:list():select(2) end)
-vim.keymap.set('n', '<Leader>h3', function () harpoon:list():select(3) end)
-vim.keymap.set('n', '<Leader>h4', function () harpoon:list():select(4) end)
-vim.keymap.set('n', '<Leader>h5', function () harpoon:list():select(5) end)
+for i = 1, 5 do
+    vim.keymap.set('n', '<Leader>h' .. i, function () harpoon:list():select(i) end)
+end
 
 local conf = require('telescope.config').values
 local function telescope_harpoon(files)
